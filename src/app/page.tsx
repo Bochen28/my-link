2f6fbdb9303bf17b321df79b77a7c6644a066d56
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-09-16 04:37:55 +0200

    Added modal for loading your links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.sass";
 
 function Home() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <main className={styles.main}>
       <h1>Welcome to My Link</h1>
-      <a>Get Started</a>
+      <a className={styles.mainBtn} href="/home">
+        Get Started
+      </a>
+      <a
+        className={styles.mainBtn}
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Load your Links
+      </a>
+
+      {isModalOpen ? (
+        <>
+          <div
+            className={styles.backdrop}
+            onClick={() => {
+              setModalOpen(false);
+            }}
+          ></div>
+          <div className={styles.modal}>
+          <h2>Insert Your Save</h2>
+            <div
+              className={styles.xButton}
+              onClick={() => {
+                setModalOpen(false);
+              }}
+            ></div>
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
+        </>
+      ) : null}
     </main>
   );
 }
