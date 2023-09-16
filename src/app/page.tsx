3aa7e56456ebd24d6e8b1b9da547173caf5e0185
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-09-16 04:51:01 +0200

    Fixed Merging problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { useState } from "react";
@@ -6,22 +5,10 @@ import styles from "./page.module.sass";
 
 function Home() {
   const [isModalOpen, setModalOpen] = useState(false);
-=======
-'use client'
-
-import { useState } from "react";
-import GetStarted from "@/components/getStartedModal/getStarted";
-import styles from "./page.module.sass";
-
-function Home() {
-
-  const [isOpen, setIsOpen] = useState(false)
->>>>>>> ab3a821d82033b1069481c89e36354c597f994cc
 
   return (
     <main className={styles.main}>
       <h1>Welcome to My Link</h1>
-<<<<<<< HEAD
       <a className={styles.mainBtn} href="/home">
         Get Started
       </a>
@@ -43,7 +30,7 @@ function Home() {
             }}
           ></div>
           <div className={styles.modal}>
-          <h2>Insert Your Save</h2>
+            <h2>Insert Your Save</h2>
             <div
               className={styles.xButton}
               onClick={() => {
@@ -55,10 +42,6 @@ function Home() {
           </div>
         </>
       ) : null}
-=======
-      <button className={styles.getStarted} onClick={() => {setIsOpen(!isOpen)}}>Get Started</button>
-      {isOpen && <GetStarted />}
->>>>>>> ab3a821d82033b1069481c89e36354c597f994cc
     </main>
   );
 }
